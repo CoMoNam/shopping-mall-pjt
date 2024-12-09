8e94d86024e3afca4198ab4baa982ba18553b337
tderk <identify25@gmail.com>
2024-12-09 16:03:20 +0900

    backend / frontend basic commit

---
+++ shopping-mall-backend/src/main/java/side/shopping/shopping_mall_backend/ShoppingPjtBackendApplication.java
@@ -0,0 +1,13 @@
+package side.shopping.shopping_mall_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingPjtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingPjtBackendApplication.class, args);
+	}
+
+}
---
+++ shopping-mall-backend/src/test/java/side/shopping/shopping_mall_backend/ShoppingPjtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package side.shopping.shopping_mall_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingPjtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
