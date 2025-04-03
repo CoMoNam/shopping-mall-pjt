a892e5c84f1a139893d9b64b5ac3660d83e0a87c
tderk <identify25@gmail.com>
2025-04-04 05:51:29 +0900

    nodemodules 까지..

--- build-prod-stage.sh
+++ build-prod-stage.sh
@@ -1,16 +1,19 @@
-# 1. 프론트엔드 디렉토리로 이동
+set -e
+
 cd shopping-mall-frontend
 
-# 2. .next 제거하고 새로 빌드
 rm -rf .next
+
 npm run build:prod
 
-# 3. 불필요한 디렉토리 제거 (LFS 문제 방지)
+
 rm -rf .next/cache
 rm -rf .next/static/development
 rm -rf .next/trace
 
-# 4. Git에 필요한 파일만 추가
+rm -rf node_modules
+npm install --omit=dev
+
 cd ..
 git add shopping-mall-frontend/.next
 
