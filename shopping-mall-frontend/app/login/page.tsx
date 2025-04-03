fa5bb3541c7751241a71d01858b1111d9bb29295
tderk <identify25@gmail.com>
2025-04-04 02:32:23 +0900

    etc

--- shopping-mall-frontend/app/login/page.tsx
+++ shopping-mall-frontend/app/login/page.tsx
@@ -61,7 +61,7 @@ const Login = () => {
             color: "inherit", // Typography 색상 상속
           }}
         >
-          SIDEMALL5
+          SIDEMALL
         </span>
       </Typography>
       <Paper
